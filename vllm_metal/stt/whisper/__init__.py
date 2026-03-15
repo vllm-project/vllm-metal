4e7236fb3d116c961349fc1ca6d372b74563d0fc
Yuan Lik Xun <lxyuan0420@gmail.com>
2026-03-15 11:23:01 +0800

    Refactor STT: package Whisper model
    Signed-off-by: Yuan Lik Xun <lxyuan0420@gmail.com>

---
+++ vllm_metal/stt/whisper/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+"""Whisper STT implementation (model-owned package)."""
+
+from .model import WhisperConfig, WhisperModel
+
+__all__ = [
+    "WhisperConfig",
+    "WhisperModel",
+]
